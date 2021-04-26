a3306eaf8325c85651ec006e47b3d09ab839f9d6
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-04-26 12:24:58 +0100

    Returning related data from an external class

--- Data/CampProfile.cs
+++ Data/CampProfile.cs
@@ -14,7 +14,28 @@ namespace CoreCodeCamp.Data
             //This is how you map between two classes, it works great especially when their names are similiar
             //By using mapping, you can create a class and populate it with the methods you'd like to expose to the client and then run operations using that Model and send it to the client.
             //The profile class is where these linkages are declared
-            this.CreateMap<Camp, CampModel>();
+
+            //Code
+            //this.CreateMap<Camp, CampModel>();
+
+            //The line of code above is different from the line of code below because it makes it easier to map to different properties in entity classes that are different from the mapped entity and model class
+
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(c => c.VenueName, o => o.MapFrom(m => m.Location.VenueName));
+            this.CreateMap<Camp, CampModel>()
+               .ForMember(c => c.Address1, o => o.MapFrom(m => m.Location.Address1));
+            this.CreateMap<Camp, CampModel>()
+               .ForMember(c => c.Address2, o => o.MapFrom(m => m.Location.Address2));
+            this.CreateMap<Camp, CampModel>()
+               .ForMember(c => c.Address3, o => o.MapFrom(m => m.Location.Address3));
+            this.CreateMap<Camp, CampModel>()
+               .ForMember(c => c.CityTown, o => o.MapFrom(m => m.Location.CityTown));
+            this.CreateMap<Camp, CampModel>()
+               .ForMember(c => c.StateProvince, o => o.MapFrom(m => m.Location.StateProvince));
+            this.CreateMap<Camp, CampModel>()
+               .ForMember(c => c.PostalCode, o => o.MapFrom(m => m.Location.PostalCode));
+            this.CreateMap<Camp, CampModel>()
+               .ForMember(c => c.Country, o => o.MapFrom(m => m.Location.Country));
         }
     }
 }
--- Models/CampModel.cs
+++ Models/CampModel.cs
@@ -12,5 +12,21 @@ namespace CoreCodeCamp.Models
         public string Moniker { get; set; }
         public DateTime EventDate { get; set; } = DateTime.MinValue;
         public int Length { get; set; } = 1;
+        public string VenueName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string CityTown { get; set; }
+        public string StateProvince { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+
+        //When you want to pull in properties from different classes (that are different from the model class origin) into the model class you must prefix the property name with the name of the external class so automapper can map to it and get the necessary data.
+
+        //eg public string LocationPostalCode { get; set; } is getting PostalCode property from Location entity class.
+        //Automapper will know this because of the Location prefix.
+
+        //Note that if you do not like the way the property names look when the API is fetched there are other ways to go about it.
+        //Adjust it in the Profile class of the model, the profile class is where you map the entity class to the model class.
     }
 }
