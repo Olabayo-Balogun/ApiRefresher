336e71c9a3f2f31337cba81e4c981773a3e0e392
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-04-27 12:37:45 +0100

    Creating a functional API

---
+++ Controllers/OperationsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        //Note that this is a functional API
+        //The other APIs we built are Restful APIs
+        //Note that you shouldn't build too many functional APIs. 
+        //Building too many functional APIs is a sign that you're entering the world of remote procedure calls
+        private readonly IConfiguration _config;
+
+        public OperationsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpOptions("reloadConfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)_config;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
